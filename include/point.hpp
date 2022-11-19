b33c4ffedcd984191b4f5b356d190fdc0879feaf
Mukund <mukund199823@gmail.com>
2022-11-19 19:16:27 +0800

    first commit

---
+++ include/point.hpp
@@ -0,0 +1,26 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+#include <string>
+//Point2D is a building block for Vector2D
+
+struct Point2D
+{
+     double x; 
+     double y;   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //POINT_HPP
+
