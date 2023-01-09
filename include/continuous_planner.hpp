88472f32d69172d5cc7030d674ddeada6d6246ac
mukund <mukund199823@gmail.com>
2023-01-09 22:39:34 +0800

    continuous planner class created. Todo

---
+++ include/continuous_planner.hpp
@@ -0,0 +1,15 @@
+#ifndef CONTINUOUS_PLANNER_HPP
+#define CONTINUOUS_PLANNER_HPP
+
+enum class PlannerState
+{
+    NotStarted,
+    Planning,
+    PathFound,
+};
+
+class ContinuousPlanner
+{
+    
+}
+#endif //CONTINUOUS_PLANNER_HPP
